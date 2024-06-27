7f54e373968fd2eea39ce169b5ff8094be39e6e5
Glenda Santana <glendasantana2099@gmail.com>
2024-06-27 20:30:42 -0300

    finished the initial and login page. now is missing the functions to connect with the controller

--- api-front/src/app/app.component.ts
+++ api-front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { PrincipalComponent } from "./principal/principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
