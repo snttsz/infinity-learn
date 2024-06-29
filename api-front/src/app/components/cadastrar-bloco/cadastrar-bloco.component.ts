1fa0afa6ca5c9fd056cb92a38097d5501ae31dc8
Glenda Santana <glendasantana2099@gmail.com>
2024-06-29 18:37:34 -0300

    api-front -> added part 1 and 2 of registering

--- api-front/src/app/components/cadastrar-bloco/cadastrar-bloco.component.ts
+++ api-front/src/app/components/cadastrar-bloco/cadastrar-bloco.component.ts
@@ -1,9 +1,10 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastrar-bloco',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cadastrar-bloco.component.html',
   styleUrl: './cadastrar-bloco.component.css'
 })
@@ -12,8 +13,34 @@ export class CadastrarBlocoComponent
 {
   @Output() buttonClick = new EventEmitter<void>();
 
+  partOfRegister: number = 1;
+
   onVoltarButtonClick(): void
   {
-    this.buttonClick.emit();
+    if (this.partOfRegister == 1)
+    {
+      this.buttonClick.emit();
+    }
+    else
+    {
+      this.partOfRegister--;
+    }
+  }
+
+  onContinuarButtonClick(): void
+  {
+    if (this.validarDadosInseridos())
+    {
+      this.partOfRegister++;
+    }
+    else
+    {
+      
+    }
+  }
+
+  validarDadosInseridos(): boolean
+  {
+    return true;
   }
 }
